99db85130e14a783e5adf5b3c59e3b6d94aca50c
AleksandrKovalenko <alexqw007@gmail.com>
2018-04-01 20:58:59 +0300

    first init

---
+++ app/src/main/java/com/example/alex/testdaggermy/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.alex.testdaggermy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.alex.testdaggermy.mySimple.AppInject;
+import com.example.alex.testdaggermy.mySimple.Body;
+import com.example.alex.testdaggermy.mySimple.Car;
+import com.example.alex.testdaggermy.mySimple.Engine;
+import com.example.alex.testdaggermy.mySimple.Wheels;
+
+import javax.inject.Inject;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String TAG = "car";
+//    @Inject Body body;
+//    @Inject Wheels wheels;
+//    Engine engine;
+    Car carNoDagger;  //without Dagger
+    @Inject Car carDagger; //with Dagger
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        /**Case1 - without Dagger */
+//        Body body = new Body();
+//        Wheels wheels = new Wheels();
+//        Engine engine = new Engine();
+//
+//        carNoDagger = new Car(body, wheels, engine);
+//        carNoDagger.print();
+//
+//        /**Case2 - without Dagger new way - inner classes */
+//        carNoDagger = new Car(new Body(),new Wheels(), new Engine());
+//        carNoDagger.print();
+
+        /**Case3 - with Dagger */
+        AppInject.getComponent().inject(this);
+        carDagger.print();
+
+        //        Log.d(TAG, "onCreate: " +   body.toString());
+//        Log.d(TAG, "onCreate: " +   wheels.toString());
+//        Log.d(TAG, "onCreate: " +   engine.toString());
+
+//        body.print();
+//        engine.print();
+    }
+}
